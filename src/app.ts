564829958a7c974eba636d235bd429399b3c23a2
defDimych <def.dimych@gmail.com>
2024-10-19 23:44:26 +0300

    Add test-router

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,13 @@ import express from 'express';
 import {SETTINGS} from "./utils/settings";
 import {getBlogsRouter} from "./routes/blogs-router";
 import {getPostsRouter} from "./routes/posts-router";
+import {getTestingRouter} from "./routes/test-router";
 
 // Create app
 export const app = express();
 
 app.use(express.json());
+
 app.use(SETTINGS.PATH.BLOGS, getBlogsRouter());
 app.use(SETTINGS.PATH.POSTS, getPostsRouter());
+app.use('/testing/all-data', getTestingRouter());
---
+++ src/routes/test-router.ts
@@ -0,0 +1,8 @@
+import express from "express";
+
+export const getTestingRouter = () => {
+    const router = express.Router();
+
+    router.delete('')
+    return router;
+}
