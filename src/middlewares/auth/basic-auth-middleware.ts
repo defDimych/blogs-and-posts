a4d48e85123e0f6858cf67cf1de995febfa4a395
defDimych <def.dimych@gmail.com>
2024-10-20 10:58:54 +0300

    Refactor

--- src/middlewares/auth/basic-auth-middleware.ts
+++ src/middlewares/auth/basic-auth-middleware.ts
@@ -2,16 +2,20 @@ import {NextFunction, Request, Response} from "express";
 import {SETTINGS} from "../../utils/settings";
 import {HTTP_STATUSES} from "../../utils/http-statuses";
 
-export const basicAuthMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    const buffer1 = Buffer.from(SETTINGS.CREDENTIALS, 'utf8');
-    const codedAuth = buffer1.toString('base64');
+export const fromUTF8ToBase64 = (credentials: string) => {
+    const buffer1 = Buffer.from(credentials, 'utf8');
+    return buffer1.toString('base64');
+}
 
+export const basicAuthMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const inputAuth = req.headers['authorization'] as string;
 
     if (!inputAuth) {
         res.sendStatus(HTTP_STATUSES.UNAUTHORIZED_401);
         return;
     }
+
+    const codedAuth = fromUTF8ToBase64(SETTINGS.CREDENTIALS);
 
     if ('Basic ' + codedAuth === inputAuth) {
         next();
