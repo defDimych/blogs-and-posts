4b0ca2c6decf94c6ec0062ff17dcb6762aecdbbd
defDimych <def.dimych@gmail.com>
2024-10-21 23:18:19 +0300

    add cors package

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,13 @@ import {SETTINGS} from "./utils/settings";
 import {getBlogsRouter} from "./routes/blogs-router";
 import {getPostsRouter} from "./routes/posts-router";
 import {getTestingRouter} from "./routes/test-router";
+import cors from "cors";
 
 // Create app
 export const app = express();
 
 app.use(express.json());
+app.use(cors())
 
 app.use(SETTINGS.PATH.BLOGS, getBlogsRouter());
 app.use(SETTINGS.PATH.POSTS, getPostsRouter());
