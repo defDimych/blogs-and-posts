e43c9d06b88d40a3a45c93bd0e39d95f3501c2fc
defDimych <def.dimych@gmail.com>
2024-10-25 23:07:29 +0300

    Refactor and add Mapper

--- src/repositories/blogs-db-repository.ts
+++ src/repositories/blogs-db-repository.ts
@@ -1,42 +1,71 @@
 import {ExtendedBlogViewModel} from "../types/blogs-types/BlogViewModel";
 import {BlogInputModel} from "../types/blogs-types/BlogInputModel";
 import {blogsCollection} from "./db";
-import {ObjectId} from "mongodb";
+import {BlogDbModel} from "../types/blogs-types/BlogDbModel";
+import {ObjectId, WithId} from "mongodb";
+
+const blogMapper = (blog: WithId<BlogDbModel>): ExtendedBlogViewModel => {
+    return {
+        id: blog._id.toString(),
+        name: blog.name,
+        description: blog.description,
+        websiteUrl: blog.websiteUrl,
+        createdAt: blog.createdAt,
+        isMembership: blog.isMembership
+    }
+}
+
 
 export const blogsRepository = {
     async getAllBlogs(): Promise<ExtendedBlogViewModel[]> {
-        return await blogsCollection.find().toArray();
+        const blogs: WithId<BlogDbModel>[] = await blogsCollection.find({}).toArray();
+        return blogs.map(blogMapper)
     },
 
-    async createBlog({ name, description, websiteUrl }: BlogInputModel): Promise<ExtendedBlogViewModel> {
+    async createBlog({ name, description, websiteUrl }: BlogInputModel): Promise<ExtendedBlogViewModel | null> {
         const newBlog = {
-            _id: new ObjectId(),
-            id: Date.now() + Math.random() + '',
             name,
             description,
             websiteUrl,
             createdAt: new Date().toISOString(),
             isMembership: false
         }
-        await blogsCollection.insertOne(newBlog);
-        return newBlog;
+        const result = await blogsCollection.insertOne(newBlog);
+        const blogId = result.insertedId.toString();
+
+        const createdBlog = blogsRepository.findBlogById(blogId);
+
+        if(createdBlog) {
+            return createdBlog;
+        } else {
+            throw new Error(`при создани блога с данными ${newBlog}, он почему то не вернулся`)
+        }
     },
 
     async findBlogById(id: string): Promise<ExtendedBlogViewModel | null> {
-        return await blogsCollection.findOne({ id });
+        const objId = new ObjectId(id);
+        const blog: WithId<BlogDbModel> | null = await blogsCollection.findOne({ _id: objId });
+
+        if (blog) {
+            return blogMapper(blog);
+        } else {
+            return null;
+        }
     },
 
     async updateBlog(id: string, {name, description, websiteUrl}: BlogInputModel): Promise<boolean> {
+        const objId = new ObjectId(id);
         const result = await blogsCollection.updateOne(
-            { id }, { $set: { name, description, websiteUrl } }
+            { _id: objId }, { $set: { name, description, websiteUrl } }
         )
 
         return result.modifiedCount === 1;
     },
 
     async deleteBlog(id: string): Promise<boolean> {
-        const result = await blogsCollection.deleteOne({ id });
+        const objId = new ObjectId(id);
+        const result = await blogsCollection.deleteOne({ _id: objId });
 
         return result.deletedCount === 1;
-    },
+    }
  }
--- src/repositories/posts-db-repository.ts
+++ src/repositories/posts-db-repository.ts
@@ -1,15 +1,30 @@
 import {ExtendedPostViewModel} from "../types/posts-types/PostViewModel";
 import {PostInputModel} from "../types/posts-types/PostInputModel";
 import {postsCollection} from "./db";
+import {ObjectId, WithId} from "mongodb";
+import {PostDbModel} from "../types/posts-types/PostDbModel";
+
+const postMapper = (post: WithId<PostDbModel>): ExtendedPostViewModel => {
+    return {
+        id: post._id.toString(),
+        title: post.title,
+        shortDescription: post.shortDescription,
+        content: post.content,
+        blogId: post.blogId,
+        blogName: post.blogName,
+        createdAt: post.createdAt
+    }
+}
 
 export const postsRepository = {
     async getAllPosts(): Promise<ExtendedPostViewModel[]> {
-        return await postsCollection.find().toArray();
+        const posts: WithId<PostDbModel>[] = await postsCollection.find({}).toArray();
+
+        return posts.map(postMapper);
     },
 
-    async createPost({ title, shortDescription, content, blogId }: PostInputModel): Promise<ExtendedPostViewModel> {
+    async createPost({ title, shortDescription, content, blogId }: PostInputModel): Promise<ExtendedPostViewModel | null> {
         const newPost = {
-            id: Date.now() + Math.random() + '',
             title,
             shortDescription,
             content,
@@ -17,25 +32,41 @@ export const postsRepository = {
             blogName: 'IT-INCUBATOR',
             createdAt: new Date().toISOString()
         }
-        await postsCollection.insertOne(newPost);
+        const result = await postsCollection.insertOne(newPost);
+        const postId = result.insertedId.toString();
 
-        return newPost;
+        const createdPost = postsRepository.findPostById(postId);
+
+        if (createdPost) {
+            return createdPost;
+        } else {
+            throw new Error(`при создани поста с данными ${newPost}, он почему то не вернулся`)
+        }
     },
 
     async findPostById(id: string): Promise<ExtendedPostViewModel | null> {
-        return await postsCollection.findOne({ id });
+        const objId = new ObjectId(id);
+        const post: WithId<PostDbModel> | null = await postsCollection.findOne({ _id: objId});
+
+        if (post) {
+            return postMapper(post);
+        } else {
+            return null;
+        }
     },
 
     async updatePost(id: string, { title, shortDescription, content, blogId }: PostInputModel): Promise<boolean> {
+        const objId = new ObjectId(id);
         const result = await postsCollection.updateOne(
-            { id }, { $set: {title, shortDescription, content, blogId} }
+            { _id: objId }, { $set: {title, shortDescription, content, blogId} }
         );
 
         return result.modifiedCount === 1;
     },
 
     async deletePost(id: string): Promise<boolean> {
-        const result = await postsCollection.deleteOne({ id });
+        const objId = new ObjectId(id);
+        const result = await postsCollection.deleteOne({ _id: objId });
 
         return result.deletedCount === 1;
     }
